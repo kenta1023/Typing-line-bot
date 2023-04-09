6ca1d04c536914816b84e3ac770b281db8af4814
kenta <c.kenta2018@gmail.com>
2023-04-09 17:59:03 +0900

    オウム返し

--- main.go
+++ main.go
@@ -1,1 +1,45 @@
+package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+func main() {
+	http.HandleFunc("/", lineHandler)
+
+}
+
+func lineHandler(w http.ResponseWriter, r *http.Request) {
+	bot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	events, err := bot.ParseRequest(r)
+	if err != linebot.ErrInvalidSignature {
+		w.WriteHeader(400)
+	} else {
+		w.WriteHeader(500)
+	}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+
+			case *linebot.TextMessage:
+				replyMessage := message.Text
+				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				if err != nil {
+					log.Print(err)
+				}
+			}
+
+		}
+	}
+}
